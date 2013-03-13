2d58057bf9eedf948a123a9ac035edadee97e48e
James Tomasino <james@tomasino.org>
2013-03-13 12:18:44 -0400

    very close to getting fonts embedding

--- js/background.js
+++ js/background.js
@@ -1,11 +1,11 @@
 
 /*************************************************
 
-	BG.js
-	------
-	This code is executed on the BACKGROUND
-	It runs in the context of the extension (not the page)
-	Is fired when the extension loads and it's always running
+BG.js
+------
+This code is executed on the BACKGROUND
+It runs in the context of the extension (not the page)
+Is fired when the extension loads and it's always running
 
 *************************************************/
 
@@ -13,52 +13,28 @@
 // UI
 //------------------------------------------------
 
-	//var textMode = false;
-	var iconOn = "../imgs/iconOn.png";
-	var iconOff = "../imgs/iconOff.png";
+//var textMode = false;
+var iconOn = "../imgs/iconOn.png";
+var iconOff = "../imgs/iconOff.png";
 
-	// Called when the user clicks on the browser action.
-	chrome.browserAction.onClicked.addListener(function(tab) {
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+	toggleIsEnableAll();
+	setListeners();
+	updateUI();
+	refreshTab(tab.id);
+});
 
-		//alert("click: "+getIsEnableAll());
-		//textMode = !textMode;
-
-		toggleIsEnableAll();
-
-		setListeners();
-		updateUI();
-		refreshTab(tab.id);
-		//chrome.tabs.reload(tab.id);
-
-		/*
-		chrome.tabs.executeScript(
-			tab.id,
-			{code:"document.body.style.background='red !important'"},
-			null
-			);
-		*/
-		/*
-			chrome.tabs[tab].executeScript(
-				null,
-				{code:"document.body.style.background='red !important'"}
-			);
-			//chrome.tabs[tab].executeScript(
-			//	null,
-			//	{code:"document.body.style.background='red !important'"}
-			//);
-		*/
-
-	});
 function updateUI() {
 	var isEnabled = getIsEnableAll();
-
-	//alert("updateUI.isEnabled: "+isEnabled);
 	var iconCurr = isEnabled ? iconOn : iconOff;
 	chrome.browserAction.setIcon({path:iconCurr});
 }
+
 function refreshTab(tabId) {
 	chrome.tabs.reload(tabId);
 }
+
 //------------------------------------------------
 // MODE
 //------------------------------------------------
@@ -66,21 +42,17 @@ function getIsEnableAll()
 {
 	return localStorage['enable_all'] === "true";
 }
+
 function setIsEnableAll(enable)
 {
 	localStorage['enable_all'] = enable;
 	return enable;
 }
+
 function toggleIsEnableAll()
 {
 	return setIsEnableAll(!getIsEnableAll());
 }
-
-//var isEnabled = false;
-//var enableAll = localStorage['enable_all'] === "true";
-//var enableAll = getIsEnableAll();
-//alert(enableAll);
-//	isEnabled = enableAll;
 
 //------------------------------------------------
 // POPUP >> PAGE
@@ -90,16 +62,15 @@ function toggleIsEnableAll()
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 	var response = {};
 
-    if (request.method === "getMode"){
-      //response.enableAll = localStorage['enable_all'];
-      response.enableAll = getIsEnableAll().toString();
+	if (request.method === "getMode"){
+	  response.enableAll = getIsEnableAll().toString();
 	}
-    if (request.refresh === "true"){
+	if (request.refresh === "true"){
 		setListeners();
 		updateUI();
 	}
 
-    sendResponse(response); // snub them.
+	sendResponse(response); // snub them.
 });
 
 //------------------------------------------------
@@ -107,98 +78,49 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 //------------------------------------------------
 
 // Sets the listeners only if the extension is enabled for the current context
-		onBeforeRequestImage = function(info)
-		{
-			//console.log("IMG intercepted: " + info.url);
-			// Redirect the image request to blank.
-			return {redirectUrl: chrome.extension.getURL("imgs/bg_blank_1px.png")};
-		};
-		onBeforeRequestObject = function(info) {
-			//console.log("IMG intercepted: " + info.url);
+onBeforeRequestImage = function(info)
+{
+	// Redirect the image request to blank.
+	return {redirectUrl: chrome.extension.getURL("imgs/bg_blank_1px.png")};
+};
 
-			// Canceling the request shows an ugly Chrome message
-			//return {cancel:true};
+onBeforeRequestObject = function(info) {
+	// Redirect the asset request to ////.
+	return {redirectUrl: chrome.extension.getURL("imgs/bg_lines_03_grey.png")};
+};
 
-			// Redirect the asset request to ////.
-			return {redirectUrl: chrome.extension.getURL("imgs/bg_lines_03_grey.png")};
-		};
 function setListeners(){
 	var isEnabled = getIsEnableAll();
 
 	if (!isEnabled)
 	{
-		chrome.webRequest.onBeforeRequest.removeListener(
-			onBeforeRequestImage
-			);
-		chrome.webRequest.onBeforeRequest.removeListener(
-			onBeforeRequestObject
-			);
+		chrome.webRequest.onBeforeRequest.removeListener(onBeforeRequestImage);
+		chrome.webRequest.onBeforeRequest.removeListener(onBeforeRequestObject);
 	}
 	else
-	//if (isEnabled)
-	//if(1)
 	{
-		//alert("setListeners");
-
 		chrome.webRequest.onBeforeRequest.addListener(
-			onBeforeRequestImage,
-					
-			//		function(info) {
-			//			console.log("IMG intercepted: " + info.url);
-			//			// Redirect the image request to blank.
-			//			return {redirectUrl: chrome.extension.getURL("imgs/bg_blank_1px.png")};
-			//		},
-					
-			// filters
-			{
+			onBeforeRequestImage, {
 				urls: [
 					"http://*/*",
 					"https://*/*"
 				],
-
-				// Possible values:
-				// "main_frame", "sub_frame", "stylesheet", "script",
-				// "image", "object", "xmlhttprequest", "other"
 				types: ["image"]
 			},
-			// extraInfoSpec
 			["blocking"]
 		);
 
 		chrome.webRequest.onBeforeRequest.addListener(
-			onBeforeRequestObject,
-					/*
-					function(info) {
-						console.log("IMG intercepted: " + info.url);
-
-						// Canceling the request shows an ugly Chrome message
-						//return {cancel:true};
-
-						// Redirect the asset request to ////.
-						return {redirectUrl: chrome.extension.getURL("imgs/bg_lines_03_grey.png")};
-					},
-					*/
-			// filters
-			{
+			onBeforeRequestObject, {
 				urls: [
 					"http://*/*",
 					"https://*/*"
 				],
-
-				// Possible values:
-				// "main_frame", "sub_frame", "stylesheet", "script",
-				// "image", "object", "xmlhttprequest", "other"
-
-				// Gmail breaks if we block IFRAMES so we block at TAB level (in tab.js)
-				//types: ["sub_frame", "object"]
 				types: ["object"]
 			},
-			// extraInfoSpec
 			["blocking"]
 		);
-
 	}
-
 }
 setListeners();
 updateUI();
